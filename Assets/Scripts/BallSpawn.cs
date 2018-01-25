96f408408e8008b79e797e6cfd73865ff895e468
gianluca monti <gm3871@gmail.com>
2018-01-25 17:34:14 +0100

    Motore di fisica ok, da fare il tuning

---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+	
+	public GameObject Prefab;
+	public Transform playerTransform;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void onMouseDown () {
+		Debug.Log("CLICK!");
+		GameObject obj=Instantiate(Prefab,playerTransform.position, Quaternion.identity) as GameObject;
+	}
+}
---
+++ Assets/Scripts/EasyTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EasyTrigger : MonoBehaviour {
+
+	private SteamVR_TrackedController device;
+	public GameObject Prefab;
+	public Transform playerTransform;
+	void Start()
+	{
+		device = GetComponent<SteamVR_TrackedController>();
+		device.TriggerClicked += Trigger;
+	}
+
+	void Trigger (object sender, ClickedEventArgs e)
+	{
+		Debug.Log("Trigger has been pressed");
+		GameObject obj=Instantiate(Prefab,playerTransform.position, Quaternion.identity) as GameObject;
+	}
+}
