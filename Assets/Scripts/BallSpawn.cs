88acd97ae8fed25868dafff22b64f6d4e65d3137
gianluca monti <gm3871@gmail.com>
2018-01-29 16:08:26 +0100

    ...

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -11,26 +11,20 @@ namespace Valve.VR.InteractionSystem {
 		public GameObject Prefab;
 		public Transform playerTransform;
 
-
 		// Use this for initialization
 		void Start () {
-			//Prefab = GameObject.Find("ThrowableBall");
-			//playerTransform = GameObject.Find("BallSpawnPoint").transform;
-
 		}
 
 		// Update is called once per frame
 		void Update () {
-			
 		}
 
 		private void HandHoverUpdate( Hand hand )
 		{
 			if ( hand.GetStandardInteractionButtonDown() || ( ( hand.controller != null ) && hand.controller.GetPressDown( Valve.VR.EVRButtonId.k_EButton_Grip ) ) )
 			{
-				Debug.Log("Pressed left click.");
-				Debug.Log("CLICK!");
 				GameObject obj=Instantiate(Prefab,playerTransform.position, Quaternion.identity) as GameObject;
+				hand.controller.TriggerHapticPulse(2500);
 			}
 		}
 	}
