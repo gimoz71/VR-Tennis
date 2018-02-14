5877631f1e9c0351661fa1ca42a0fda4da9ec1d6
gianluca monti <gm3871@gmail.com>
2018-02-14 13:50:17 +0100

    aggiunta collisori e tuning vari

---
+++ Assets/Scripts/DetectArea.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectArea : MonoBehaviour {
+
+
+    public Text myText;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+       // Debug.Log("Collided with " + other.gameObject.name);
+       if (other.gameObject.name == "AreaAnterioreDX")
+        {
+            Debug.Log("AreaAnterioreDX");
+            myText.text = "AreaAnterioreDX";
+        }
+        if (other.gameObject.name == "AreaAnterioreSX")
+        {
+            Debug.Log("AreaAnterioreSX");
+            myText.text = "AreaAnterioreSX";
+        }
+        if (other.gameObject.name == "AreaPosterioreDX")
+        {
+            Debug.Log("AreaPosterioreDX");
+            myText.text = "AreaPosterioreDX";
+        }
+        if (other.gameObject.name == "AreaPosterioreDX")
+        {
+            Debug.Log("AreaPosterioreDX");
+            myText.text = "AreaPosterioreDX";
+        }
+        if (other.gameObject.name == "CollisioneRete")
+        {
+            Debug.Log("CollisioneRete");
+            myText.text = "CollisioneRete";
+        }
+    }
+}
--- Assets/Scripts/UI/ToggleDifficulty.cs
+++ Assets/Scripts/UI/ToggleDifficulty.cs
@@ -6,36 +6,66 @@ public class ToggleDifficulty : MonoBehaviour {
 
 	public GameObject _target;
 
-	public Toggle easy;
-	public Toggle medium;
-	public Toggle hard;
-	// Use this for initialization
+	public Toggle livello1;
+	public Toggle livello2;
+	public Toggle livello3;
+    public Toggle livello4;
 
-	void Start() {
+    public GameObject targetParent;
+    public GameObject targetLivello1;
+    public GameObject targetLivello2;
+    public GameObject targetLivello3;
+    public GameObject targetLivello4;
+
+    private Quaternion targetRotation;
+
+    private int _min = -2;
+    private int _max = 2;
+
+    // Use this for initialization
+
+    void Start() {
 		
 	}
 	public void ActiveToggle () {
-		if (easy.isOn) {
-			_target.transform.position = GameObject.Find("area1lento").transform.position;
-			Debug.Log("EASY");
 
-		} else if (medium.isOn) {
-			_target.transform.position = GameObject.Find("area1medio").transform.position;
-			Debug.Log("MEDIUM");
-		} else if (hard.isOn) {
-			_target.transform.position = GameObject.Find("area1veloce").transform.position;
-			Debug.Log("HARD");
-		}
-	}
+        targetRotation = Quaternion.Euler(0, Random.Range(_min, _max), 0);
+        targetParent.transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 15.0f);
+
+        if (livello1.isOn) {
+			_target.transform.position = targetLivello1.transform.position;
+			Debug.Log("Livello1");
+            _min = -4;
+            _max = 4;
+
+		} else if (livello2.isOn) {
+			_target.transform.position = targetLivello2.transform.position;
+			Debug.Log("Livello2");
+            _min = -8;
+            _max = 8;
+
+        } else if (livello3.isOn) {
+			_target.transform.position = targetLivello3.transform.position;
+			Debug.Log("Livello3");
+            _min = -12;
+            _max = 12;
+
+        } else if (livello4.isOn) {
+            _target.transform.position = targetLivello4.transform.position;
+            Debug.Log("Livello4");
+            _min = -16;
+            _max = 16;
+
+        }
+    }
 
 	public void OnSubmit () {
-		Debug.Log("Selected difficulty:");
-
 		ActiveToggle();
 	}
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       
+    }
 }
  
