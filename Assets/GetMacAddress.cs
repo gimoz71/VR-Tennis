c0c03ec99d208dc201affbeb80c679bc45b6798c
gimoz71 <gm3871@gmail.com>
2018-07-26 17:38:29 +0200

    aggiunto controllo MAC address

--- Assets/GetMacAddress.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Net.NetworkInformation;
-
-var macAddr = 
-	(
-		from nic in NetworkInterface.GetAllNetworkInterfaces()
-		where nic.OperationalStatus == OperationalStatus.Up
-		select nic.GetPhysicalAddress().ToString()
-	).FirstOrDefault();
---
+++ Assets/Scripts/GetMacAddress.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.NetworkInformation;
+using System.Linq;
+
+public class GetMacAddress : MonoBehaviour {
+
+	public Text mac;
+	public Button startButton;
+	// Use this for initialization
+	void Start () {
+		var macAddr =
+			(
+				from nic in NetworkInterface.GetAllNetworkInterfaces()
+				where nic.OperationalStatus == OperationalStatus.Up
+				select nic.GetPhysicalAddress().ToString()
+			).FirstOrDefault();
+		
+
+		if (macAddr.Equals( "0023AE83B346")) {
+			Debug.Log ("--------------------OK---------------> " + macAddr);
+			mac.text = "";
+			startButton.interactable = true;
+		} else {
+			Debug.Log ("--------------------ERROR---------------> " + macAddr);
+			mac.text = "Computer non abilitato";
+			startButton.interactable = false;
+		}
+	}
+}
