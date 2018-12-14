3520dfe9fbe902ffe881836476a1f5f7aee9c34c
Gianluca Monti <gm3871@gmail.com>
2018-12-14 17:19:20 +0100

    migliorie varie

--- Assets/Scripts/GetMacAddress.cs
+++ Assets/Scripts/GetMacAddress.cs
@@ -10,28 +10,54 @@ public class GetMacAddress : MonoBehaviour
 
     public Text mac;
     public Button startButton;
+
+    public static string MAC_ADDRESS_SVILUPPO = "C86000CC99C2";
+    public static string MAC_ADDRESS_CLIENTE = "0C5415DB124E";
+
     // Use this for initialization
     void Start()
     {
-        var macAddr =
-            (
+        var addresses = (
                 from nic in NetworkInterface.GetAllNetworkInterfaces()
                     // where nic.OperationalStatus == OperationalStatus.Up // forza il dispositivo ad essere connesso
                 select nic.GetPhysicalAddress().ToString()
-            ).FirstOrDefault();
+            ).ToList();
 
-        // 9CEBE8B0B294 -0C5415DB124F - 0C5415DB124E - 0C5415DB1252 --> portatile Gioele
-        // C86000CC99C2 --> PC-VR ufficio
-        if (macAddr.Equals("9CEBE8B0B294") || macAddr.Equals("0C5415DB124F") || macAddr.Equals("0C5415DB124E") || macAddr.Equals("0C5415DB1252") || macAddr.Equals("C86000CC99C2") || macAddr.Equals("0023AE83B346") || macAddr.Equals("0649EC638AE3") || macAddr.Equals("000272389453"))
+        if (addresses.IndexOf(MAC_ADDRESS_CLIENTE) != -1 || addresses.IndexOf(MAC_ADDRESS_SVILUPPO) != -1)
         {
-            Debug.Log("--------------------OK---------------> " + macAddr);
-            mac.text = "";
+            
+            //trovato
+            Debug.Log("--------------------OK---------------> " + addresses);
+            mac.text = ""/* + addresses*/;
             startButton.interactable = true;
         }
-        else {
-            Debug.Log("--------------------ERROR---------------> " + macAddr);
-            mac.text = "PC non abilitato";
+        else
+        {
+            //non trovato
+            Debug.Log("--------------------ERROR---------------> " + addresses);
+            mac.text = "PC non abilitato: "/* + addresses*/;
             startButton.interactable = false;
         }
+
+        //var macAddr =
+        //    (
+        //        from nic in NetworkInterface.GetAllNetworkInterfaces()
+        //            // where nic.OperationalStatus == OperationalStatus.Up // forza il dispositivo ad essere connesso
+        //        select nic.GetPhysicalAddress().ToString()
+        //    ).FirstOrDefault();
+
+        // 9CEBE8B0B294 -0C5415DB124F - 0C5415DB124E - 0C5415DB1252 - 0E5415DB124E --> portatile Gioele
+        // C86000CC99C2 --> PC-VR ufficio
+        //if (macAddr.Equals("9CEBE8B0B294") || macAddr.Equals("0C5415DB124F") || macAddr.Equals("0E5415DB124E") || macAddr.Equals("0C5415DB124E") || macAddr.Equals("0C5415DB1252") || macAddr.Equals("C86000CC99C2") || macAddr.Equals("0023AE83B346") || macAddr.Equals("0649EC638AE3") || macAddr.Equals("000272389453"))
+        //{
+        //    Debug.Log("--------------------OK---------------> " + macAddr);
+        //    mac.text = ""/* + macAddr*/;
+        //    startButton.interactable = true;
+        //}
+        //else {
+        //    Debug.Log("--------------------ERROR---------------> " + macAddr);
+        //    mac.text = "PC non abilitato"/* + macAddr*/;
+        //    startButton.interactable = false;
+        //}
     }
 }
